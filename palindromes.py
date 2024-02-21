6dd36baa6b76e0c14bcd4abb87cf28b29d03e2b3
SurnmHanes <125546959+SurnmHanes@users.noreply.github.com>
2024-02-21 15:14:50 +0000

    Update palindromes.py

--- palindromes.py
+++ palindromes.py
@@ -1,15 +1,19 @@
 #Testing for palindromes
 
+# obtain the word / phrase from the user
 text = input("Enter your message: ")
 
-
+# remove any whitespace to convert to a string of characters and convert to upper case
 text_adj = text.replace(" ", "")
 text_adj = text_adj.upper()
 
 try:
-   # print(text_adj)
+  
+   # if it is all whitespace i.e. nothing entered confirm not a palindrome
     if text == " ":
         print( "It's not a palindrome" )
+  
+   # reverse character string and compare to non-reversed version
     elif text_adj[::-1]  == text_adj:
         print( "It's a palindrome" )
     else:
