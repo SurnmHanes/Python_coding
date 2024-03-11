2d827b79fadabe6f57d5b82883db96e0bcc31a0a
SurnmHanes <125546959+SurnmHanes@users.noreply.github.com>
2024-03-11 10:09:21 +0000

    Add files via upload

--- ball_by_ball.py
+++ ball_by_ball.py
@@ -12,12 +12,15 @@ def extract_to_csv(d):
     df = pd.DataFrame(d['innings'])
     df_dates = pd.DataFrame( d['info']['dates'])
     team_list = d['info']['teams']
+    match_type = d['info']['match_type']
+    #print(fname)
+    
 
     all_innings = []
     
     # first check that England is one of the teams involved. Otherwise can just skip.
     # df['overs'] is the ball by ball list per innings. So we need to iterate over each of the innings
-    if 'England' in team_list:    
+    if 'England' in team_list and match_type == "Test":    
         
         for i in range( len(df['overs'])):
         
@@ -34,15 +37,22 @@ def extract_to_csv(d):
             df_innings['innings'] = i + 1
             df_innings['startdate'] = df_dates[0][0]
             df_innings['team'] = df['team'][i]
+            opposition_list = [x for x in team_list if x != df['team'][i]]
+            df_innings['opposition'] = opposition_list[0]
+            
             
             # add innings to all_innings list
             all_innings.append(df_innings)
 
     # all_innings is a list of dataframes which we then concatenate: 
         whole_test = pd.concat(all_innings)
-    
+       
+    # obtain the test number and add to the whole_test dataframe
+        match_type_number = d['info']['match_type_number']
+        whole_test['test_number'] = match_type_number
+
     # export to csv
-        whole_test.to_csv('WholeMatch.csv', index=False, mode='a', header=['batter', 'bowler', 'non_striker', 'runs.batter', 'runs.total', 'wickets', 'innings', 'startdate', 'team'])
+        whole_test.to_csv('WholeMatch.csv', index=False, mode='a', header=['batter', 'bowler', 'non_striker', 'runs.batter', 'runs.total', 'wickets', 'innings', 'startdate', 'team', 'opposition', 'test_number'])
 
 # %%
 
@@ -51,6 +61,8 @@ for fname in glob("C:/Users/NeilHanes/Downloads/tests_male_json/*.json"):
     with open(fname, 'r') as json_data:
         d = json.load(json_data)
         extract_to_csv(d)
+
+print('action completed')
 
 
 # %%
